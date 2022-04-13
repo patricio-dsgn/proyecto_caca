9532460fb5d0cf68970f7cbdfcee332ccdf22777
patricio-dsgn <patricio.ngg@gmail.com>
2022-04-13 18:02:04 -0400

    add material

--- escala_bristol/urls.py
+++ escala_bristol/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('contact', views.contact),
+    path('history', views.history),
+    # path('', views.index, name='index'),
+    path('', views.scale),
+    path('interpretation', views.interpretation),
+    path('scale', views.scale),
+    path('uses', views.uses),
 ]
--- escala_bristol/views.py
+++ escala_bristol/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
 def index(request):
     return render(request,'escala_bristol/index.html')
 
-def contact(request):
-    return render(request,'escala_bristol/contact.html')
+def history(request):
+    return render(request,'escala_bristol/history.html')
+
+
+def uses(request):
+    return render(request,'escala_bristol/uses.html')
+
+
+def scale(request):
+    return render(request,'escala_bristol/scale.html')
+
+def scale2(request):
+    return render(request,'escala_bristol/doxa.html')
+
+
+def interpretation(request):
+    return render(request,'escala_bristol/interpretation.html')
+
